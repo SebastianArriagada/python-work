79275379b1e13897dd2926bfe8ca94fc3dab114f
Sebastian Arriagada <sarriagada15@gmail.com>
2022-01-14 19:18:53 +0100

    Add test results

--- codility/SparseBinaryDecomposition.py
+++ codility/SparseBinaryDecomposition.py
@@ -23,3 +23,48 @@ def solution(N):
             return num
         
     return -1
+
+"""
+
+Task Score
+50%
+Correctness
+80%
+Performance
+20%
+
+    
+Detected time complexity:
+O(N * log(N))
+expand allExample tests
+▶example1
+example test n=26✔OK
+expand allCorrectness tests
+▶simple1
+n=1166✔OK
+▶simple2
+n=561892✔OK
+▶simple3
+n=1031✔OK
+▶small_power_of_two_minus_one
+n=1023✔OK
+▶extreme
+n <= 5✘WRONG ANSWER
+got -1, but solution exists for example 1 (1=1+0)
+expand allPerformance tests
+▶medium1
+n=74901729✘TIMEOUT ERROR
+running time: 0.252 sec., time limit: 0.144 sec.
+▶medium2
+n=216188401✘TIMEOUT ERROR
+running time: 3.580 sec., time limit: 0.144 sec.
+▶power_of_two_minus_one
+n=536870912✔OK
+▶big_random
+n=~1000000000✘TIMEOUT ERROR
+Killed. Hard limit reached: 6.000 sec.
+▶maximal
+n=1000000000✘TIMEOUT ERROR
+Killed. Hard limit reached: 6.000 sec.    
+    
+"""
