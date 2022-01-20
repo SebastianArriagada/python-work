98e41b9300343f1131791fe7734bcf1b9934ab9a
Sebastian Arriagada <sarriagada15@gmail.com>
2022-01-20 21:39:26 +0100

    Create folder for store Hacker Rank exercises

---
+++ hackerRank/SparseArrays.py
@@ -0,0 +1,46 @@
+"""
+There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+
+Example
+
+
+
+There are  instances of ',  of '' and  of ''. For each query, add an element to the return array, .
+
+Function Description
+
+Complete the function matchingStrings in the editor below. The function must return an array of integers representing the frequency of occurrence of each query string in strings.
+
+matchingStrings has the following parameters:
+
+string strings[n] - an array of strings to search
+string queries[q] - an array of query strings
+Returns
+
+int[q]: an array of results for each query
+Input Format
+
+The first line contains and integer , the size of .
+Each of the next  lines contains a string .
+The next line contains , the size of .
+Each of the next  lines contains a string .
+
+"""
+
+def matchingStrings(strings, queries):
+    
+    result = []
+    
+    for query in queries:
+        
+        appearances = 0
+                
+        for string in strings:
+               
+            if query == string:
+                appearances += 1
+    
+        result.append(appearances)
+    
+    return result
+        
---
+++ hackerRank/TimeConversion.py
@@ -0,0 +1,62 @@
+"""
+Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+
+
+Return '12:01:00'.
+
+
+Return '00:01:00'.
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+string s: a time in  hour format
+Returns
+
+string: the time in  hour format
+Input Format
+
+A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+Constraints
+
+All input times are valid
+Sample Input
+
+07:05:45PM
+Sample Output
+
+19:05:45
+
+"""
+
+def timeConversion(s):
+    
+    stringLeght = len(s)
+    
+    horary = s[stringLeght - 2: stringLeght]
+    hourInt = int(s[0:2])
+    
+    if horary == "AM":
+        if hourInt == 12:
+            hourString = "00" + s[2:8]
+            return hourString
+        else:
+            return s[0:8]
+    
+    if hourInt == 12:
+        hourString = "12" + s[2:8]
+    else:
+        hourInt += 12
+        hourString = str(hourInt) + s[2:8]
+    
+    return hourString
+    
